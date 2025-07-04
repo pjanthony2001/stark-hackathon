0c60e735400d1d8016388231406bd71fa9238e3c
Guillaume Bardinet <guillaume.bardinet@etu.minesparis.psl.eu>
2025-07-04 10:07:30 +0200

    Classe state_machine en cours d'amelioration

--- utils/state_machine.py
+++ utils/state_machine.py
@@ -1,10 +1,35 @@
+# # 
+
 import numpy as np
+from matrix import Matrix
+from field import Field, Fieldelement
 
 
 class State_machine:
+    def computational_trace_1(x0:'Matrix', A:'Matrix', T:int, F:'Field')->'Matrix':
+        # For relationships x_{i+1}=A x_i
+        # x0 is a vector, A is a matrix, T is the number of iterations
+        w=len(x0.values)
+        xi=x0
+        zero=Fieldelement(F,0)
+        liste_de_zeros=[zero]*(T*w)
+        matrix=Matrix(T,w,liste)
+        for i in range(T):
+            for j in range(w):
+                matrix.matrix[i][j]=xi.values[j]
+                matrix.values[i*w+j]=xi.values[j]
+            xi=A.dot(xi)
+        return matrix
+
+    def polynome_j_degree1(j:int, x:'Matrix', y:'Matrix', A:'Matrix')->float:
+        Aj=Matrix(1,A.columns,A.matrix[j])
+        return y.values[j]-Aj.dot(x)
+
+
+class State_machine_en_cours_de_correction:
     
     def __init__(self):
-        
+        pass
     
     def computational_trace_1(x0:np.ndarray, A:np.ndarray, T:int)->np.ndarray:
         # For relationships x_{i+1}=A x_i
@@ -44,5 +69,4 @@ class State_machine:
             Aj=A[j]
             P=P-np.dot(Aj,(x**i))
         return P
-
 
